d49cd1dd6d5ce15ac0f0b56a12648fa40f20664d
abarabone <abara00@live.jp>
2019-06-05 01:27:53 +0900

    a

--- Assets/10_script/TexturePacker.cs
+++ Assets/10_script/TexturePacker.cs
@@ -9,42 +9,43 @@ namespace Abss.Geometry
 {
 	public static class TexturePacker
 	{
-		
-		//public static Texture PackTextureAndTranslateUv( IEnumerable<GameObject> targetObjects )
-		//{
-		//	var mmts = MeshExtractionUtility.QueryMeshMatsTransform_IfHaving( targetObjects ).ToList();
 
-		//	var matHashToIdxDict = ( from x in mmts select x.mats )
-		//		.To(MaterialUtility.QueryMatNameAndHash_EverySubmeshesEveryMeshes)
-		//		.To(MaterialUtility.ToDictionaryForMaterialHashToIndex);
+		public static Texture PackTextureAndTranslateUv( IEnumerable<GameObject> targetObjects )
+		{
+			var mmts = FromObject.QueryMeshMatsTransform_IfHaving( targetObjects ).ToList();
 
-		//	var vtxCount_EverySubmeshes = ( from x in mmts select x.mesh )
-		//		.To(VertexUtility.QueryVertexCount_EverySubmeshes);
+			var matHashToIdxDict = ( from x in mmts select x.mats )
+				.To( MaterialUtility.QueryMatNameAndHash_EverySubmeshesEveryMeshes )
+				.To( MaterialUtility.ToDictionaryForMaterialHashToIndex );
+			var matHash
 
-		//	var qFlatMats =
-		//		from x in mmts
-		//		from mat in x.mats
-		//		select mat
-		//		;
+			var vtxCount_PerSubMeshPerMesh = ( from x in mmts select x.mesh )
+				.To( PerSubMeshPerMesh.QueryVertexCount );
 
-		//	var qTextures =
-		//		from mat in qFlatMats
-		//		select mat.mainTexture
-		//		;
-		//	//var atlas = new Texture2D( width:0, height:0, textureFormat:TextureFormat.ARGB32, mipChain:false );
-		//	var dstTexture = new Texture2D( 0, 0 );
-		//	var flatUvRects = dstTexture.PackTextures(
-		//		qTextures.Cast<Texture2D>().ToArray(), padding: 0, maximumAtlasSize: 4096,
-		//		makeNoLongerReadable: false
-		//	);
+			var qFlatMats =
+				from x in mmts
+				from mat in x.mats
+				select mat
+				;
 
-		//	//var q =
-		//	//	from x in (qFlatMats, flatUvRects).Zip( (x,y)=>(mat:x, uvs:y) )
-		//	//	select 
+			var qTextures =
+				from mat in qFlatMats
+				select mat.mainTexture
+				;
+			//var atlas = new Texture2D( width:0, height:0, textureFormat:TextureFormat.ARGB32, mipChain:false );
+			var dstTexture = new Texture2D( 0, 0 );
+			var flatUvRects = dstTexture.PackTextures(
+				qTextures.Cast<Texture2D>().ToArray(), padding: 0, maximumAtlasSize: 4096,
+				makeNoLongerReadable: false
+			);
+
+			//var q =
+			//	from x in (qFlatMats, flatUvRects).Zip( (x,y)=>(mat:x, uvs:y) )
+			//	select 
 
 
-		//	return null;
-		//}
+			return null;
+		}
 
 	}
 }
